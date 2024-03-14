7fee251a5c874d344dcea7743933a1f63a58a0f8
BUNJEE <vsolomatin@kse.org.ua>
2024-03-14 14:38:46 +0200

    1403 classwork

--- query2_bonus_create.sql
+++ query2_bonus_create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE locations_copy LIKE locations;
 CREATE INDEX location_id ON locations_copy (id);
+CREATE INDEX location_name ON locations_copy (name);
 
 INSERT INTO locations_copy(name, country, region, latitude, longitude)
 SELECT name, country, region, latitude, longitude FROM locations;
